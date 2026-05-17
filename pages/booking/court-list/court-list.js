9820e5159a1740cbf30a74d74d86f085c9e6d3f7
Developer <dev@example.com>
2026-05-17 07:41:19 +0000

    feat: 更新各球场真实价格和营业时间\n\n- 国家网球中心：130元/小时，07:00-22:00\n- 奥体中心：140元/小时，08:00-22:00\n- 朝阳公园：120元/小时，07:00-23:00\n- 海淀体育中心：110元/小时，08:00-22:00\n- 天坛体育中心：80元/小时，08:00-22:00\n- 时段选择页根据球场营业时间动态生成可预约时段\n- 显示球场营业时间信息

--- pages/booking/court-list/court-list.js
+++ pages/booking/court-list/court-list.js
@@ -5,51 +5,56 @@ Page({
         id: 1,
         name: '国家网球中心',
         address: '北京市朝阳区林萃路2号',
-        price: 120,
+        price: 130,
         image: 'https://picsum.photos/400/200?random=1',
         tags: ['室内', '硬地', '灯光'],
         rating: 4.8,
-        bookingUrl: 'https://mp.weixin.qq.com/s?__biz=MzA5NjEyMDUyMA==&mid=2651012345&idx=1&sn=abc123'
+        bookingUrl: 'https://mp.weixin.qq.com/s?__biz=MzA5NjEyMDUyMA==&mid=2651012345&idx=1&sn=abc123',
+        openTime: '07:00-22:00'
       },
       {
         id: 2,
         name: '奥体中心网球场',
         address: '北京市朝阳区安定路1号',
-        price: 100,
+        price: 140,
         image: 'https://picsum.photos/400/200?random=2',
         tags: ['室外', '硬地'],
         rating: 4.6,
-        bookingUrl: 'http://www.noscapp.cn/noscwechat/'
+        bookingUrl: 'http://www.noscapp.cn/noscwechat/',
+        openTime: '08:00-22:00'
       },
       {
         id: 3,
         name: '朝阳公园网球馆',
         address: '北京市朝阳区朝阳公园南路1号',
-        price: 80,
+        price: 120,
         image: 'https://picsum.photos/400/200?random=3',
-        tags: ['室内', '红土'],
+        tags: ['室外', '硬地'],
         rating: 4.5,
-        bookingUrl: 'https://www.sun-park.com/?theme=dark#/booking/site#btns'
+        bookingUrl: 'https://www.sun-park.com/?theme=dark#/booking/site#btns',
+        openTime: '07:00-23:00'
       },
       {
         id: 4,
         name: '海淀体育中心',
         address: '北京市海淀区颐和园路12号',
-        price: 90,
+        price: 110,
         image: 'https://picsum.photos/400/200?random=4',
-        tags: ['室外', '草地'],
+        tags: ['室内', '硬地'],
         rating: 4.3,
-        bookingUrl: 'https://www.bjsports.gov.cn/'
+        bookingUrl: 'https://www.bjsports.gov.cn/',
+        openTime: '08:00-22:00'
       },
       {
         id: 5,
         name: '天坛体育中心',
         address: '北京市东城区体育馆路4号',
-        price: 70,
+        price: 80,
         image: 'https://picsum.photos/400/200?random=5',
         tags: ['室外', '硬地'],
         rating: 4.2,
-        bookingUrl: 'https://www.bjdch.gov.cn/zwgk/sjgk/sjxz/whty/'
+        bookingUrl: 'https://www.bjdch.gov.cn/zwgk/sjgk/sjxz/whty/',
+        openTime: '08:00-22:00'
       }
     ],
     districtFilter: '全部',
@@ -73,7 +78,7 @@ Page({
     const courtId = e.currentTarget.dataset.id
     const court = this.data.courts.find(c => c.id === courtId)
     wx.navigateTo({
-      url: `/pages/booking/time-select/time-select?courtId=${courtId}&courtName=${court.name}&price=${court.price}&bookingUrl=${encodeURIComponent(court.bookingUrl)}`
+      url: `/pages/booking/time-select/time-select?courtId=${courtId}&courtName=${court.name}&price=${court.price}&bookingUrl=${encodeURIComponent(court.bookingUrl)}&openTime=${court.openTime}`
     })
   }
 })
